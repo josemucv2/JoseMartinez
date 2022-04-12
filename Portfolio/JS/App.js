095c7853cb0641f174b70550fb28bcc450cfd476
josemucv2 <martinezcaceresjosem@gmail.com>
2022-04-12 00:23:29 -0400

    page

---
+++ Portfolio/JS/App.js
@@ -0,0 +1,30 @@
+$(document).ready(() => {
+
+    const width =  screen.width;
+      
+    if(width < 768){
+        $(".openMain").click(() =>{
+            $('.desactive').slideToggle('fast');
+            
+        })
+
+
+        $('.nav-bar-mobile').css('display', 'block');
+        $(".add-et").append('<br/>')
+
+
+    }
+
+
+
+  const height = screen.height
+  console.log(height)
+
+
+
+
+
+})
+
+
+
