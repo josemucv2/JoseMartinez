cfe20e8ee5279e4d5892a1b6014889e6387da822
josemucv2 <martinezcaceresjosem@gmail.com>
2022-04-12 17:43:01 -0400

    icons

--- Portfolio/JS/App.js
+++ Portfolio/JS/App.js
@@ -1,27 +1,31 @@
 $(document).ready(() => {
-
-    const width =  screen.width;
-     
-      
+     $('.disclaimer').css('display', 'none');
+    const width = $(window).width();
+    console.log(width);
     if(width < 768){
         $(".openMain").click(() =>{
             $('.desactive').slideToggle('fast');
-            
+            $('main').css('margin-top','120px');
         })
-
-
+        
         $('.nav-bar-mobile').css('display', 'block');
         $(".add-et").append('<br/>')
-       
+    }
 
-
+    $('.fa-whatsapp').hover(
+        function(){ 
+            $(this).append($('<button>Send Message</button>'));
+           
+            
     }
     
+    , function(){
+        $(this).find('button').remove();
+        
+    })
+    
 
-
-  const height = screen.height
-  console.log(height)
-  $('.disclaimer').css('display', 'none');
+ 
 
 
 })
