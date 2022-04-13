d5c7c0faa62efaf78dfae23fb5a398575712c50c
josemucv2 <martinezcaceresjosem@gmail.com>
2022-04-13 03:31:33 -0400

    final

--- Portfolio/JS/App.js
+++ Portfolio/JS/App.js
@@ -1,8 +1,11 @@
 $(document).ready(() => {
-     $('.disclaimer').css('display', 'none');
+    
+    $('.disclaimer').css('display', 'none');
+    
     const width = $(window).width();
-    console.log(width);
+    
     if(width < 768){
+        
         $(".openMain").click(() =>{
             $('.desactive').slideToggle('fast');
             $('main').css('margin-top','120px');
@@ -10,24 +13,55 @@ $(document).ready(() => {
         
         $('.nav-bar-mobile').css('display', 'block');
         $(".add-et").append('<br/>')
+        $('.sub-title-developer').append('Desarrollo Web')
+        $('.sub-title-javascript').append('Javascript')
+        $('.sub-title-React').append('React.js')
     }
-
-    $('.fa-whatsapp').hover(
+    
+    $('.project-ecommerce').hover(
         function(){ 
-            $(this).append($('<button>Send Message</button>'));
-           
-            
-    }
-    
-    , function(){
-        $(this).find('button').remove();
+            $(this).append($('<a href="https://projectcoderecommerce.netlify.app" class="go-to-page"><button class="button-image">Go to Page</button></a>'));
+        },
         
-    })
-    
-
- 
-
-
+        function(){
+            $(this).find('a').remove();
+        })
+        
+        $('.project-landingPage').hover(
+            function(){ 
+                $(this).append($('<a href="https://josemucv2.github.io/landingpage/" class="go-to-page"><button class="button-image">Go to Page</button></a>'));
+            },
+            
+            function(){
+                $(this).find('a').remove();
+            })
+            
+    $('.img-developer').hover(
+        function(){ 
+            $(this).append($('<a href="https://latam.coderhouse.com/certificados/6130e1c8b05a7a00115695c8" class="go-to-page"><button class="button-image">Go to Page</button></a>'));
+        },
+        
+        function(){
+            $(this).find('a').remove();
+        })
+        
+        $('.img-javascript').hover(
+            function(){ 
+                $(this).append($('<a href="https://latam.coderhouse.com/certificados/618feed74bd4ae00271927b5" class="go-to-page"><button class="button-image">Go to Page</button></a>'));
+            },
+        
+        function(){
+            $(this).find('a').remove();
+        })
+        
+        $('.img-React').hover(
+            function(){ 
+                $(this).append($('<a href="https://latam.coderhouse.com/certificados/6213cd53adfa9b003f5bd763" class="go-to-page"><button class="button-image">Go to Page</button></a>'));
+            }, 
+            
+            function(){
+                $(this).find('a').remove();
+            })
 })
 
 
