a8fd37d96781bd76000610c8a8a6c9d4248e4ee4
josemucv2 <martinezcaceresjosem@gmail.com>
2022-05-22 00:05:03 -0400

    changes projects

--- Portfolio/JS/App.js
+++ Portfolio/JS/App.js
@@ -44,38 +44,61 @@ $(document).ready(() => {
                 $(this).find('a').remove();
                 $(this).text('Landing Page');
             })
+
+            $('.project-Loginuser').hover(
+            function(){ 
+                $(this).text('');
+                $(this).append($('<a href="https://loginuserproject.netlify.app" class="go-to-page"><button class="button-image" style="background-color: green">Go to Page</button></a>'));
+            },
+            
+            function(){
+                $(this).find('a').remove();
+                $(this).text('Login User');
+            })
             
     $('.img-developer').hover(
         function(){ 
-            $(this).append($('<a href="https://latam.coderhouse.com/certificados/6130e1c8b05a7a00115695c8" class="go-to-page"><button class="button-image" style="background-color: green">Go to Page</button></a>'));
+            $(this).text('')
+            $(this).append($(`<a href="https://latam.coderhouse.com/certificados/6130e1c8b05a7a00115695c8" class="go-to-page"><button class="button-image" style="background-color: green">Go to Page</button></a>`));
         },
         
         function(){
             $(this).find('a').remove();
+            $(this).text('Certificate Developer Web');
         })
         
         $('.img-javascript').hover(
             function(){ 
+                $(this).text('');
                 $(this).append($('<a href="https://latam.coderhouse.com/certificados/618feed74bd4ae00271927b5" class="go-to-page"><button class="button-image" style="background-color: green">Go to Page</button></a>'));
             },
         
         function(){
             $(this).find('a').remove();
+            $(this).text('Certificate Developer Web');
         })
         
         $('.img-React').hover(
             function(){ 
+                $(this).text('');
                 $(this).append($('<a href="https://latam.coderhouse.com/certificados/6213cd53adfa9b003f5bd763" class="go-to-page"><button class="button-image" style="background-color: green">Go to Page</button></a>'));
             }, 
             
             function(){
                 $(this).find('a').remove();
+                $(this).text('Certificate on React.js');
             })
+
+            $('.react').hover(
+            function(){ 
+                $(this).text('');
+                $(this).append($('<a href="https://latam.coderhouse.com/certificados/62578aa4bcf2cf0019a13ed5" class="go-to-page"><button class="button-image" style="background-color: green">Go to Page</button></a>'));
+            }, 
             
-   
-            
-            
-            
+            function(){
+                $(this).find('a').remove();
+                $(this).text('Certificate Developer Front End on React.js');
+            })
         })
 
 
